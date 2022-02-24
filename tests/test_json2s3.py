38783ecb4d153d50b485a39a29a175e706d6bb58
zelenadinja <zelenadinja@gmail.com>
2022-02-24 17:56:18 +0100

    flake8

--- tests/test_json2s3.py
+++ tests/test_json2s3.py
@@ -4,12 +4,13 @@ import pytest
 from movie_titles_to_s3 import json_to_s3
 
 
-@pytest.mark.parametrize("object_body, object_key",
-[
-    ([1, 2, 3], "test/list.json"),
-    ({"a":1, "b":2, "c":3}, "test/dict.json"),
-    ((1, 2, 3), "tests/tuple.json"),
-])
+@pytest.mark.parametrize(
+    "object_body, object_key",
+    [
+        ([1, 2, 3], "test/list.json"),
+        ({"a": 1, "b": 2, "c": 3}, "test/dict.json"),
+        ((1, 2, 3), "tests/tuple.json"),
+    ])
 def test_json_to_s3(object_body, object_key):
     """Test uploading varios obects to s3 bucket"""
 
