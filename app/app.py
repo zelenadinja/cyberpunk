ed65704d470f84c66dd7133f91d69760d7839ef7
zelenadinja <zelenadinja@gmail.com>
2022-02-28 09:10:52 +0100

    container for app

--- app/app.py
+++ app/app.py
@@ -15,17 +15,21 @@ app = Flask(__name__)
 
 @app.route("/",  methods=['GET', 'POST'])
 def scheduled_image():
-    return render_template(template_name_or_list='scheduled_image.html')
+
+    image = "https://cyberpunk-web-app.s3.eu-west-3.amazonaws.com/POST/cyberpunk_SCHEDULED"
+    return render_template(template_name_or_list='scheduled_image.html', scheduled_image=image)
 
 
 @app.route('/GENERATED', methods=['GET', 'POST'])
 def generated_image():
+
     LAMBDA_GENERATE.invoke(
         FunctionName=LAMBDA_ARN,
         InvocationType='RequestResponse',
     )
-    return render_template(template_name_or_list='generated_image.html')
+    image = "https://cyberpunk-web-app.s3.eu-west-3.amazonaws.com/POST/cyberpunk_GENERATED"
+    return render_template(template_name_or_list='generated_image.html', generated_image=image)
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000)
